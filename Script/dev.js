cf3c2ff7d47a2e093c8c60fc6a475dc6c6c1af2f
Funtimeswithdanny <dannymorrisey1@outlook.com>
2024-12-07 17:21:04 -0500

    New Update

---
+++ Script/dev.js
@@ -0,0 +1,32 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  // Get the button
+let mybutton = document.getElementById("myBtn");
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function myFunction1() {
+var element = document.body;
+element.classList.toggle("dark-mode");
+}
